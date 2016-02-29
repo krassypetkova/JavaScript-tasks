b91b93d3a822b104ada87ed9c0590cfa1f5c5bdb
krassypetkova <krassypetkova@gmail.com>
2016-02-29 18:34:56 +0200

    Added ex4 in conditional statements with functions

--- ConditionalStatementsWithFunctions/numbersDescendingOrder.js
+++ ConditionalStatementsWithFunctions/numbersDescendingOrder.js
@@ -1,3 +1,9 @@
-/**
- * Created by KrassyPetkova on 2/29/2016.
- */
+/*4. Sort 3 real numbers in descending order. Use nested if statements. */
+
+function numsDescendingOrder() {
+    var args = [].slice.apply(arguments);
+
+    console.log(args.sort(function(a, b){return a-b}));
+}
+
+numsDescendingOrder(4, 80, 100, 67, 63, 5, 1);
